bc40db64aaad29af5025853a919320f5397fafbe
Armando Di Cianno <armando@dicianno.org>
2015-04-30 09:00:29 -0400

    Add UINavigationControllerDelegate to NVC

--- TestNVC/NavController.m
+++ TestNVC/NavController.m
@@ -6,17 +6,21 @@
 //
 //
 
+
 #import "NavController.h"
+#import "ViewController.h"
 
-@interface NavController ()
 
+@interface NavController () <UINavigationControllerDelegate>
 @end
+
 
 @implementation NavController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +28,24 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)navigationController:(UINavigationController *)navigationController
+      willShowViewController:(UIViewController *)viewController
+                    animated:(BOOL)animated
+{
+    ViewController *vc = (ViewController *)viewController;
+    if (vc.level > 0) {
+        NSLog(@"-[nvcD willShowViewController]: %@", vc.title);
+    }
 }
-*/
+
+- (void)navigationController:(UINavigationController *)navigationController
+       didShowViewController:(UIViewController *)viewController
+                    animated:(BOOL)animated
+{
+    ViewController *vc = (ViewController *)viewController;
+    if (vc.level > 0) {
+        NSLog(@"-[nvcD didShowViewController]: %@", vc.title);
+    }
+}
 
 @end
